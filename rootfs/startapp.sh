ceee51291082444b4e280729b640e6e57ef42c7d
Jocelyn Le Sage <jocelyn@le-sage.com>
2019-05-09 08:24:32 -0400

    Print the Firefox version during startup.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -5,4 +5,5 @@ set -u # Treat unset variables as an error.
 
 export HOME=/config
 mkdir -p /config/profile
+firefox --version
 exec /usr/bin/firefox_wrapper --profile /config/profile --setDefaultBrowser >> /config/log/firefox/output.log 2>> /config/log/firefox/error.log
