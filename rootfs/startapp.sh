2b7317ba7be536e282c96fb1a381dc364a0250b9
Fantix King <fantix.king@gmail.com>
2020-08-04 18:52:20 -0500

    pin to gen3.org for demo

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -6,4 +6,4 @@ set -u # Treat unset variables as an error.
 export HOME=/config
 mkdir -p /config/profile
 firefox --version
-exec /usr/bin/firefox_wrapper --profile /config/profile --setDefaultBrowser >> /config/log/firefox/output.log 2>> /config/log/firefox/error.log
+exec /usr/bin/firefox_wrapper --profile /config/profile --setDefaultBrowser --kiosk https://gen3.org >> /config/log/firefox/output.log 2>> /config/log/firefox/error.log
