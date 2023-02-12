d27a479a7def3ec110641641e99838ed33ba114f
Jocelyn Le Sage <jocelyn@le-sage.com>
2023-02-11 21:53:12 -0500

    Added `browser.tabs.closeWindowWithLastTab` to the default Firefox config.

--- rootfs/defaults/prefs.js
+++ rootfs/defaults/prefs.js
@@ -1,2 +1,4 @@
 // Disable the privacy notice page.
 user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+// Prevent closing Firefox when closing the last tab.
+user_pref("browser.tabs.closeWindowWithLastTab", false);
