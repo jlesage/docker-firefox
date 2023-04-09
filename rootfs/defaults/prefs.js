e1d32c30f2dae373578d29add4941ba233bb5f97
Jocelyn Le Sage <jocelyn@le-sage.com>
2023-04-08 21:18:35 -0400

    Improved graceful shutdown of Firefox:   - By default, disable confirmation when quitting using the ctrl+q shortcut.   - Wait for Firefox to quit after sending ctrl+q key presses.

--- rootfs/defaults/prefs.js
+++ rootfs/defaults/prefs.js
@@ -2,3 +2,6 @@
 user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
 // Prevent closing Firefox when closing the last tab.
 user_pref("browser.tabs.closeWindowWithLastTab", false);
+// Disable confirmation before quitting with Ctrl+Q.  Needed to allow Firefox
+// to quit cleanly when container is shutted down.
+user_pref("browser.warnOnQuitShortcut", false);
