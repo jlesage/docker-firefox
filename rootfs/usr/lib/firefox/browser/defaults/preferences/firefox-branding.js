646af130f10d2041f0b78932d0a993ce779dba98
Jocelyn Le Sage <jocelyn@le-sage.com>
2024-09-29 17:45:46 -0400

    Disabled warning message.

--- rootfs/usr/lib/firefox/browser/defaults/preferences/firefox-branding.js
+++ rootfs/usr/lib/firefox/browser/defaults/preferences/firefox-branding.js
@@ -3,6 +3,8 @@ pref("browser.download.dir", "/config/downloads");
 pref("browser.download.folderList", 2);
 // Disable the privacy notice page.
 pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+// Disable some warning messages.
+pref("security.sandbox.warn_unprivileged_namespaces", false);
 // Prevent closing Firefox when closing the last tab.
 pref("browser.tabs.closeWindowWithLastTab", false);
 // Disable confirmation before quitting with Ctrl+Q.  Needed to allow Firefox
